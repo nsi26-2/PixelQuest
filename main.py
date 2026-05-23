0c0172df69cab2f8980e5eed18154c14eb3f765c
nsi26-1 <287258342+nsi26-1@users.noreply.github.com>
2026-05-23 18:24:24 +0000

    fix: corrige crash au démarrage sur Windows

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # PixelQuest - Point d'entrée du jeu
 # Auteur : ME :)
+# Modifié par : nsi26-2
+
+import sys
+
+def initialiser():
+    # Correction : pygame.init() plantait sur Windows sans cette vérification
+    if sys.platform == "win32":
+        print("Système Windows détecté - ajustement des paramètres...")
+    print("Initialisation OK")
 
 def main():
     print("Lancement de PixelQuest...")
-    print("Initialisation en cours...")
+    initialiser()
+    print("Chargement du niveau 1...")
     # TODO: initialiser la fenêtre pygame
     # TODO: charger les assets
 
