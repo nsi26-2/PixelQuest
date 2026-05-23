783af195fca811fbaa40f35ae5dd8a619a9eb477
nsi26-2 <287259182+nsi26-2@users.noreply.github.com>
2026-05-23 16:25:13 +0000

    Ajout boucle principale du jeu

---
+++ main.py
@@ -0,0 +1,11 @@
+# PixelQuest - Point d'entrée du jeu
+# Auteur : lucas-dev42
+
+def main():
+    print("Lancement de PixelQuest...")
+    print("Initialisation en cours...")
+    # TODO: initialiser la fenêtre pygame
+    # TODO: charger les assets
+
+if __name__ == "__main__":
+    main()
