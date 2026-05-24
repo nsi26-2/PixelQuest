30bc4e06ac59f81839860711ff77d635d32ffffd
nsi26-2 <287259182+nsi26-2@users.noreply.github.com>
2026-05-24 18:03:19 +0000

    add final boss

---
+++ boss.py
@@ -0,0 +1,14 @@
+# Boss final — Work In Progress
+# Auteur : not you :-(
+
+class BossFinale:
+    def __init__(self):
+        self.nom = "Dragon Ancestral"
+        self.vie = 500
+        self.degats = 50
+        self.phase = 1
+
+    # TODO: implémenter les phases du boss
+    # TODO: ajouter les animations
+    # TODO: ajouter les drops de loot
+    pass
